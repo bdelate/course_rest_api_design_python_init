26d6600d89fee84e4d2e5ed1a8ec61c8c4803b19
bdelate <brendon.d@tangentsolutions.co.za>
2025-07-28 08:11:44 +0200

    done

--- src/api/endpoints/barks.py
+++ src/api/endpoints/barks.py
@@ -8,7 +8,7 @@ def barks_list(request):
     """
     Bark list endpoint that returns a list of barks.
     """
-    return [{"id": 1, "message": "bark 1!"}, {"id": 2, "message": "bark 2!"}, {"id": 3, "message": "bark 3!"}]
+    return [{"id": 1, "message": "bark 1!"}, {"id": 2, "message": "bark 2!"}, {"id": 3, "message": "bark 2!"}]
 
 
 @router.get("/{bark_id}/")
